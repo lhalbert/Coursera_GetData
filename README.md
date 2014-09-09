b8784a343f013a60ed4a9a247bfd131ba578b9ac
lhalbert <lhalbert@gmail.com>
2014-09-09 16:02:58 -0400

    Incremental Update

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 4. Appropriately labels the data set with descriptive variable names. 
 
 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+<h2>Study Design</h2>
+
+<h2>Code Book</h2>
--- run_analysis.R
+++ run_analysis.R
@@ -4,3 +4,19 @@
 # Date: 4 Sep 2014
 #
 ##############################################################################
+
+#Check to see if data dir exists
+if (!file.exists("data")){dir.create("data")}
+
+#Download file
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+download.file(fileUrl, "./data/file.xml", method = "curl")
+#list.files("./data")
+
+#Set date for download
+#dateDownloaded <- Date()
+
+#Read data
+colIndex <- 7:15 
+rowIndex <- 18:23 
+data <- xmlToDataFrame("./data/file.xml")
