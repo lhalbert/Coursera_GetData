3754dd6b1fd541ff33eb1b7ae59c239ff4a5ecc5
lhalbert <lhalbert@gmail.com>
2014-09-10 19:32:36 -0400

    R Script Complete

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-<h2>Create an R script called run_analysis.R that does the following. </h2>
+This repository contains R code that downloads, merges and preprocesses Human Activity Recognition data set. 
+
+
+<h2>Create an R script called run_analysis.R</h2>
 
 1. Merges the training and the test sets to create one data set.
-
 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-
 3. Uses descriptive activity names to name the activities in the data set
-
 4. Appropriately labels the data set with descriptive variable names. 
-
 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 <h2>Study Design</h2>
--- run_analysis.R
+++ run_analysis.R
@@ -5,18 +5,108 @@
 #
 ##############################################################################
 
+##############################################################################
+#Download Data
+##############################################################################
 #Check to see if data dir exists
-if (!file.exists("data")){dir.create("data")}
+if (!file.exists("data")) {
+     message("Creating data directory...")
+     dir.create("data")
+}
+if (!file.exists("data/UCI HAR Dataset")) {
+     #Download the data
+     fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+     zipfile="data/UCI_HAR_data.zip"
+     message("Downloading data...")
+     download.file(fileURL, destfile=zipfile, method="curl")
+     unzip(zipfile, exdir="data")
+}
+message("Download Data Complete!")
+message(" ")
 
-#Download file
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
-download.file(fileUrl, "./data/file.xml", method = "curl")
-#list.files("./data")
+##############################################################################
+#Read Data
+##############################################################################
+message("reading X_train.txt...")
+training.x <- read.table("data/UCI HAR Dataset/train/X_train.txt")
 
-#Set date for download
-#dateDownloaded <- Date()
+message("reading y_train.txt...")
+training.y <- read.table("data/UCI HAR Dataset/train/y_train.txt")
 
-#Read data
-colIndex <- 7:15 
-rowIndex <- 18:23 
-data <- xmlToDataFrame("./data/file.xml")
+message("reading subject_train.txt...")
+training.subject <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+
+message("reading X_test.txt...")
+test.x <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+
+message("reading y_test.txt...")
+test.y <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+
+message("reading subject_test.txt...")
+test.subject <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
+
+message("Reading Data Complete!")
+message(" ")
+
+##############################################################################
+#Merge Data
+##############################################################################
+merged.x <- rbind(training.x, test.x)
+merged.y <- rbind(training.y, test.y)
+merged.subject <- rbind(training.subject, test.subject)
+mergeData <- list(x=merged.x, y=merged.y, subject=merged.subject)
+message("Merge Data Complete!")
+message(" ")
+##############################################################################
+#Extract only the measurements on the mean & Std Dev. for @ measurement.
+##############################################################################
+#Read the feature list file
+features <- read.table("data/UCI HAR Dataset/features.txt")
+
+#Find the mean and std columns
+mean.col <- sapply(features[,2], function(x) grepl("mean()", x, fixed=T))
+std.col <- sapply(features[,2], function(x) grepl("std()", x, fixed=T))
+
+#Extract them from the data
+extractData <- mergeData$x[, (mean.col | std.col)]
+colnames(extractData) <- features[(mean.col | std.col), 2]
+
+message("Extract Measurements Complete!")
+message(" ")
+
+##############################################################################
+#Uses descriptive activity names to name the activities in the data set
+##############################################################################
+yData <- mergeData$y
+colnames(yData) <- "activity"
+yData$activity[yData$activity == 1] = "WALKING"
+yData$activity[yData$activity == 2] = "WALKING_UPSTAIRS"
+yData$activity[yData$activity == 3] = "WALKING_DOWNSTAIRS"
+yData$activity[yData$activity == 4] = "SITTING"
+yData$activity[yData$activity == 5] = "STANDING"
+yData$activity[yData$activity == 6] = "LAYING"
+
+colnames(mergeData$subject) <- c("subject")
+
+message("Activity Names Complete!")
+message(" ")
+
+# Combine mean-std values (x), activities (y) and subjects into one data
+# frame.
+
+combindData <- cbind(extractData, yData, mergeData$subject)
+
+message("Combine Measurements Complete!")
+message(" ")
+
+##############################################################################
+#Create an independent tidy dataset
+##############################################################################
+library(plyr) #tools for splitting, applying and combining data
+tidy <- ddply(combindData, .(subject, activity), 
+              function(x) colMeans(x[,1:60]))
+
+# Write tidy dataset as csv
+write.csv(tidy, "UCI_HAR_tidy_data.csv", row.names=FALSE)
+
+message("Tidy data file Complete!")
